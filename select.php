01ca60504a4368015ee2b9c44f30bf483578c486
shanebyers <shane@shane-byers.com>
2022-02-19 20:30:51 -0700

    Further work

--- select.php
+++ select.php
@@ -2,7 +2,7 @@
     echo "select.php", PHP_EOL;
     $json_body = file_get_contents('php://input');
     $body = json_decode($json_body, true);
-    $db_login = $body['databaseLogin']
+    $db_login = $body['databaseLogin'];
     $connection = new mysqli($db_login['serverName'], $db_login['username'], $db_login['password'], $db_login['databaseName']);
     if ($connection->connect_error)
     {
