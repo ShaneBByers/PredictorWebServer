7856e79126082913800ee37563d5d3fc0615fa13
shanebyers <shane@shane-byers.com>
2022-02-19 18:28:58 -0700

    Further work

--- test.php
+++ test.php
@@ -2,5 +2,5 @@
     echo "TEST.PHP", PHP_EOL;
     $json_body = file_get_contents('php://input');
     $body = json_decode($json_body, true);
-    echo SERVER_NAME:, $body['ServerName'], PHP_EOL;
+    echo "SERVER_NAME:", $body['ServerName'], PHP_EOL;
 ?>
