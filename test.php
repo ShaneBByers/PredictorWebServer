0bfd5580ae7851a1debb4e2ae7386ab507a968f7
shanebyers <shane@shane-byers.com>
2022-02-19 18:27:03 -0700

    Working on json encode and decode.

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
     echo "TEST.PHP", PHP_EOL;
-    $entityBody = file_get_contents('php://input');
-    echo $entityBody, PHP_EOL;
+    $json_body = file_get_contents('php://input');
+    $body = json_decode($json_body, true)
+    echo SERVER_NAME:, $body['ServerName'], PHP_EOL;
 ?>
