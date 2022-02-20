5e2d7d55ece0861a07dfc0ddb69a14bdbf625dff
shanebyers <shane@shane-byers.com>
2022-02-19 18:37:29 -0700

    Working on PHP database connection.

--- test.php
+++ test.php
@@ -2,5 +2,10 @@
     echo "TEST.PHP", PHP_EOL;
     $json_body = file_get_contents('php://input');
     $body = json_decode($json_body, true);
-    echo "SERVER_NAME:", $body['serverName'], PHP_EOL;
+    $connection = new mysqli($body['serverName'], $body['username'], $body['password']);
+    if ($connection->connect_error)
+    {
+        echo "Connection FAILED: ", $connection->connect_error, PHP_EOL;
+    }
+    echo "Connection SUCCEEDED", PHP_EOL;
 ?>
